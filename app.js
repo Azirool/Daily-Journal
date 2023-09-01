0fde4afe4b416cc5f4acc7f7a9dfd03092e97305
Azirool Fareed <aziroolfareed31@gmail.com>
2023-09-01 22:03:41 +0800

    Create a template for every post to open on new page

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const homeStartingContent = "MyDailyJournal is a beginner-friendly online platform designed to help you establish the habit of daily journaling. Whether you're new to journaling or a seasoned writer, our user-friendly interface makes it easy for anyone to record their thoughts, ideas, and experiences on a daily basis. Any new post that you compose will be shown down here.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
@@ -21,19 +22,34 @@ app.get("/", (req, res) => {
     content : homeStartingContent, 
     posts    : posts
   });
-})
+});
 
 app.get("/about", (req,res) => {
   res.render("about", {content:aboutContent});
-})
+});
 
 app.get("/contact", (req, res) => {
   res.render("contact", {content:contactContent});
-})
+});
 
 app.get("/compose", (req,res) => {
   res.render("compose");
-})
+});
+
+app.get("/posts/:postID", (req,res) => {
+  const urlTitle = _.lowerCase(req.params.postID);
+
+  posts.forEach((post) => {
+    const postTitle = _.lowerCase(post.title);
+    const postBody = post.body;
+    if (urlTitle === postTitle){
+      res.render("post", {
+        title : post.title,
+        body  : postBody
+      });
+    }
+  });
+});
 
 app.post("/compose", (req,res) => {
   const post = {
@@ -43,7 +59,7 @@ app.post("/compose", (req,res) => {
   posts.push(post);
 
   res.redirect("/");
-})
+});
 
 
 
