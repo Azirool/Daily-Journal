6947463c6c9ab2139c50031f182b4cd9fd1ab763
Azirool Fareed <aziroolfareed31@gmail.com>
2023-09-05 16:30:34 +0800

    Add/edit comment and readme file

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Daily-Journal
-The Daily Journal App is a simple, user-friendly journaling application that allows users to record their daily thoughts, experiences, and reflections. It provides a convenient platform for personal reflection and self-improvement.
+A simple web-based journaling application created with Node.js, Express.js, EJS, and MongoDB. Easily record and manage your daily thoughts and experiences.
+
 
 # Project Overview:
-This Daily Journal Web Project is a learning exercise that uses Node.js, Express.js, EJS, Lodash, Body-Parser, HTML, and CSS to create a simple web application for journaling. The primary goal of this project is to teach the process of creating a template-based web application using these technologies and deploying it to GitHub.
+This Daily Journal Web Project is a learning exercise that uses Node.js, Express.js, MongoDB, Mongoose, EJS, Lodash, Body-Parser, HTML, and CSS to create a simple web application for journaling. The primary goal of this project is to teach the process of creating a template-based web application using these technologies and deploying it to GitHub.
 
 # Project Features:
 
 - User Interface: The web application will have a clean and user-friendly interface for creating and viewing journal entries.
-- Journal Entries: Users can create, edit, and delete journal entries. Each entry will consist of a title and content.
+- Journal Entries: Users can create journal entries. Each entry will consist of a title and content.
 - Template Engine (EJS): We will use EJS (Embedded JavaScript) as the template engine to dynamically generate HTML content with data from the server. 
-- Express.js Backend: Express.js will be used to create a RESTful API for handling CRUD operations (Create, Read) on journal entries.
+- Express.js Backend: Express.js will be used to handle the backend of the journal entries.
 - Lodash: Lodash will be used for utility functions and simplifying data manipulation.
 - CSS Styling: The application will have CSS styles to make it visually appealing and responsive.
 - Body-Parser Middleware: Body-Parser will be used to parse incoming HTTP requests, making it easier to handle form submissions.
@@ -20,12 +21,11 @@ This Daily Journal Web Project is a learning exercise that uses Node.js, Express
 - Views: EJS templates for rendering journal entries, a home page, a compose page, an about page, a contact page and a post page.
 - App.js: The main application file where you set up the Express.js server and define routes.
 - Package.json: The project's package configuration file with dependencies and scripts.
-- README.md: A project documentation file explaining how to set up and run the application.
 
 # Learning Objectives:
 
 - Understand the basics of Node.js and Express.js for server-side development.
 - Learn how to use EJS as a template engine to dynamically render HTML pages.
-- Implement CRUD functionality for managing journal entries.
+- Understanding how MongoDB and Mongoose works.
 - Gain experience with Lodash for data manipulation.
 - Practice using Body-Parser for handling form submissions.
--- app.js
+++ app.js
@@ -6,68 +6,80 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
+const app = express();
+//EJS template
+app.set('view engine', 'ejs');
+//Body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+//CSS
+app.use(express.static("public"));
+//Database Connections
+mongoose.connect('mongodb+srv://<YourUsername>:<YourPassword>@cluster0.yb3jo2c.mongodb.net/<YourCollectionName>');
+
+//Default content
 const homeStartingContent = "MyDailyJournal is a beginner-friendly online platform designed to help you establish the habit of daily journaling. Whether you're new to journaling or a seasoned writer, our user-friendly interface makes it easy for anyone to record their thoughts, ideas, and experiences on a daily basis. Any new post that you compose will be shown down here.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-mongoose.connect('mongodb+srv://<YourUsername>:<YourPassword>@cluster0.yb3jo2c.mongodb.net/<YourCollectionName>');
-
+//New post schema
 const postSchema = mongoose.Schema({
   title: String,
   content: String
 });
-
+//Create MongoDB collection
 const Post = mongoose.model("Post", postSchema);
-
+//Home Route
 app.get("/", (req, res) => {
   Post.find({}).then((foundBlog) => {
     res.render("home", {
+      //Passing data to home.ejs file
       content : homeStartingContent, 
       posts    : foundBlog
     });
   });
 });
-
+//About Route
 app.get("/about", (req,res) => {
+    //Render page
   res.render("about", {content:aboutContent});
 });
-
+//Contact Route
 app.get("/contact", (req, res) => {
+    //Render page
   res.render("contact", {content:contactContent});
 });
-
+//Compose Route
 app.get("/compose", (req,res) => {
+  //Render page
   res.render("compose");
 });
-
+//Fetch data from MongoDB and show on their own respective page
 app.get("/posts/:postID", (req,res) => {
+  //Store post _id
   const requestedPostId  = req.params.postID;
-
+  //Fetch data from MongoDB
   Post.findOne({_id:requestedPostId}).then((result) => {
+    //Render page based on _id
     res.render("post", {
+      //Passing data to post.ejs file
       title : result.title,
       content  : result.content
     });
   });
 });
-
+//Post function for compose
 app.post("/compose", (req,res) => {
+  //Insert new data into MongoDB
   const post = new Post({
     title: req.body.composeTitle,
     content: req.body.composeBody
   })
+  //Save data into MongoDB
   post.save();
-
+  //Redirect back to home route
   res.redirect("/");
 });
 
+//Express localhost
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
